8c51176b331940d001853d3594be8f7cd006e9d8
wolfiee42 <saifalislam2022@gmail.com>
2023-11-26 22:04:43 +0600

    token added and created a middlewire name verifytoken to verify the token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+var jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,7 +13,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-
+const verifyToken = (req, res, next) => {
+    console.log(req.headers.authorization);
+    if (!req.headers.authorization) {
+        res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            res.status(401).send({ message: "access denied" })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
@@ -31,17 +44,25 @@ dbConnet();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster42.pkfz4lk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
 
 app.get('/', (req, res) => {
     res.send('brother is using harmony')
+})
+
+app.post('/jwt', async (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '365d' });
+    res.send({ token })
+
 })
 
 
